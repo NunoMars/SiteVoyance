7543b1c53e3d33c97dac4a0f6968c14d4e8b3086
NunoMars <nuno.ricardo.mars@gmail.com>
2024-01-01 23:20:05 +0100

    essai

--- clairvoyance/urls.py
+++ clairvoyance/urls.py
@@ -1,13 +1,21 @@
 from django.urls import path, re_path
 
 from django.views.decorators.csrf import csrf_exempt
-from .views import clairvoyance, clairvoyante, user_history, card_deck, card_detail
+from .views import (
+    ClairvoyanceView,
+    clairvoyante,
+    user_history,
+    CardDeckView,
+    CardDetailView,
+)
 
 
 urlpatterns = [
-    path("", clairvoyance, name="clairvoyance"),
+    path("", ClairvoyanceView.as_view(), name="clairvoyance"),
     re_path("clairvoyante", csrf_exempt(clairvoyante), name="clairvoyante"),
     path("history", user_history, name="history"),
-    path("card_deck", card_deck, name="card_deck"),
-    re_path(r"^card_detail/(?P<card>[0-9]+)/$", card_detail, name="card_detail"),
+    path("card_deck", CardDeckView.as_view(), name="card_deck"),
+    re_path(
+        r"^card_detail/(?P<card>[0-9]+)/$", CardDetailView.as_view(), name="card_detail"
+    ),
 ]
--- clairvoyance/views.py
+++ clairvoyance/views.py
@@ -1,94 +1,90 @@
 import contextlib
-from django.shortcuts import redirect, render
-from .logic import clairvoyant
+from django.shortcuts import render
 from django.http import JsonResponse
 from django.contrib.auth.decorators import login_required
+
+from clairvoyance.logic import clairvoyant
 from .models import MajorArcana
 from accounts.models import CustomUser, History, DailySortedCards
+from django.views.generic.base import TemplateView
+from django.views import View
 
 
-def index(request):
-    args = {
-        "first_title": "Benvenu/e dans mon monde",
-        "second_title": "TAROT T",
+class IndexView(TemplateView):
+    template_name = "home.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["first_title"] = "Benvenu/e dans mon monde"
+        context["second_title"] = "TAROT T"
+        return context
+
+
+class ClairvoyanceView(View):
+    template_name = "clairvoyance/clairvoyance.html"
+
+    def get(self, request, *args, **kwargs):
+        args = {"page_title": "Tarot"}
+        return render(request, self.template_name, args)
+
+
+class CardDeckView(View):
+    template_name = "clairvoyance/card_deck.html"
+
+    def get(self, request, *args, **kwargs):
+        cards = MajorArcana.objects.all()
+        lang = request.LANGUAGE_CODE
+
+        context = {
+            "lang": lang,
+            "cards": cards,
+        }
+        return render(request, self.template_name, context)
+
+
+class CardDetailView(View):
+    template_name = "clairvoyance/card_detail.html"
+    lang_fields = {
+        "fr": "fr",
+        "pt": "pt",
+        "en": "en",
+        "es": "es",
     }
-    return render(request, "home.html", args)
 
+    def get_queryset(self):
+        lang = self.request.LANGUAGE_CODE
 
-def clairvoyance(request):
-    args = {"page_title": "Tarot"}
+        field_suffix = self.lang_fields.get(lang, "fr")
+        return MajorArcana.objects.all().values(
+            f"card_name_{field_suffix}",
+            f"card_signification_gen_{field_suffix}",
+            f"card_signification_love_{field_suffix}",
+            f"card_signification_work_{field_suffix}",
+            f"card_signification_warnings_{field_suffix}",
+            "card_image",
+        )
 
-    return render(request, "clairvoyance/clairvoyance.html", args)
-
-
-def card_deck(request):
-    cards = MajorArcana.objects.all()
-    lang = request.LANGUAGE_CODE
-
-    args = {
-        "lang": lang,
-        "cards": cards,
-    }
-    return render(request, "clairvoyance/card_deck.html", args)
-
-
-def card_detail(request, card):
-    card = MajorArcana.objects.get(id=card)
-    lang = request.LANGUAGE_CODE
-    args = {}
-    if lang == "fr":
-        args["card_name"] = card.card_name_fr
-        args["card_signification_gen"] = card.card_signification_gen_fr
-        args["card_signification_love"] = card.card_signification_love_fr
-        args["card_signification_work"] = card.card_signification_work_fr
-        args[
-            "card_signification_warnings"
-        ] = card.card_signification_warnings_fr
-    if lang == "pt":
-        args["card_name"] = card.card_name_pt
-        args["card_signification_gen"] = card.card_signification_gen_pt
-        args["card_signification_love"] = card.card_signification_love_pt
-        args["card_signification_work"] = card.card_signification_work_pt
-        args[
-            "card_signification_warnings"
-        ] = card.card_signification_warnings_pt
-    if lang == "en":
-        args["card_name"] = card.card_name_en
-        args["card_signification_gen"] = card.card_signification_gen_en
-        args["card_signification_love"] = card.card_signification_love_en
-        args["card_signification_work"] = card.card_signification_work_en
-        args[
-            "card_signification_warnings"
-        ] = card.card_signification_warnings_en
-    if lang == "es":
-        args["card_name"] = card.card_name_es
-        args["card_signification_gen"] = card.card_signification_gen_es
-        args["card_signification_love"] = card.card_signification_love_es
-        args["card_signification_work"] = card.card_signification_work_es
-        args[
-            "card_signification_warnings"
-        ] = card.card_signification_warnings_es
-    args["card_image"] = card.card_image
-    return render(request, "clairvoyance/card_detail.html", args)
+    def get(self, request, card):
+        queryset = self.get_queryset()
+        if card_data := queryset.filter(id=card).first():
+            print(card_data)
+            return render(request, self.template_name, card_data)
+        else:
+            return render(request, "clairvoyance/card_not_found.html")
 
 
 def clairvoyante(request):
-    lang = request.LANGUAGE_CODE
-
     if request.method != "POST":
         return
     with contextlib.suppress(ValueError):
         input_value = request.POST.get("messageInput")
-        result = clairvoyant(input_value, lang)  
+        result = clairvoyant(input_value, request.LANGUAGE_CODE)
         return JsonResponse(result)
 
-@login_required()
-def user_history(request):
-    """Fonction for show the user's sorted cards,
-    login required."""
-    user = request.user
-    user = CustomUser.objects.get(email=user.email)
 
+@login_required
+def user_history(request):
+    user = request.user
     user_history = History.objects.filter(user=user)
     daily_user_card = DailySortedCards.objects.filter(user=user)
 
