39c7d9df4168621166f6243831f7510aa49de154
NunoMars <nuno.ricardo.mars@gmail.com>
2024-01-08 14:53:01 +0100

    last commit for test

--- clairvoyance/management/commands/migrate_database.py
+++ clairvoyance/management/commands/migrate_database.py
@@ -2,22 +2,23 @@ from django.core.management.base import BaseCommand
 from clairvoyance.models import MajorArcana
 import json
 
+
 class Command(BaseCommand):
-    help = 'Peuplate database with a json file'
-    
+    help = "Peuplate database with a json file"
+
     def handle(self, *args, **options):
-        with open('database_dumps/MajorArcana.json') as f:
+        with open("database_dumps/MajorArcana.json") as f:
             data = json.load(f)
             for i in data:
                 MajorArcana.objects.create(
-                    card_name = i["card_name"],
-                    card_signification_gen = i["card_signification_gen"],
-                    card_signification_warnings = i["card_signification_warnings"],
-                    card_signification_love = i["card_signification_love"],
-                    card_signification_work = i["card_signification_work"],
-                    card_image = i["card_image"],
-                    card_polarity = i["card_polarity"]
+                    card_name=i["card_name"],
+                    card_signification_gen=i["card_signification_gen"],
+                    card_signification_warnings=i["card_signification_warnings"],
+                    card_signification_love=i["card_signification_love"],
+                    card_signification_work=i["card_signification_work"],
+                    card_image=i["card_image"],
+                    card_polarity=i["card_polarity"],
                 )
-                print('%s" Cree en base de données!' % i["card_name"])
+                print(f'{i["card_name"]}" Cree en base de données!')
 
-        self.stdout.write(self.style.SUCCESS('Successfully !!!'))
+        self.stdout.write(self.style.SUCCESS("Successfully !!!"))
